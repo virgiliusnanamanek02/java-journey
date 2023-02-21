61164eb6685b7798d15a9d6efa1a44fe82f3a939
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-02-21 22:54:36 +0700

    #4: Conditions(if-else)

---
+++ src/nana/learning/conditions/Conditions.java
@@ -0,0 +1,15 @@
+package nana.learning.conditions;
+
+public class Conditions {
+    public static void main(String[] args) {
+        // example of conditions (if-else)
+        boolean isAlien = false;
+        if (!isAlien) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        } else {
+            System.out.println("It is an alien!");
+        }
+
+    }
+}
