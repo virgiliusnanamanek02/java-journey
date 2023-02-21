b62699081e31e9744642ac5f7d399d7a65c1cbca
Vicky Nana <virgilius.nana.manek0719@gmail.com>
2023-02-21 22:41:39 +0700

    #2: Variables

---
+++ src/nana/learning/variable/Variables.java
@@ -0,0 +1,10 @@
+package nana.learning.variable;
+
+public class Variables {
+    public static void main(String[] args) {
+        String name = "Nana";
+        int age = 20;
+
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+}
